d6153dddb6685ce2bfe847d1af55bcf2e341c91d
goztrk <me@gokhan.org.tr>
2023-11-23 14:19:50 +0300

    Adds base ZSH configuration

---
+++ .dotfiles/zsh/aliases.zsh
@@ -0,0 +1,14 @@
+alias reload!='. ~/.zshrc'
+
+# Good 'ol Clear Screen command
+alias cls='clear'
+
+# Moving up from directory
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias cd..='cd ..'
+
+alias ee='emacsclient -nw -c'
+
+alias cfg='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
