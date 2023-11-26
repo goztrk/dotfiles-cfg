d82f73177a855e7629c25668818e730715537bbb
goztrk <me@gokhan.org.tr>
2023-11-26 13:02:22 +0300

    Add tokyonight theme to neovim

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -1,4 +1,15 @@
 return {
+	{
+		'folke/tokyonight.nvim',
+		lazy = false,
+		priority = 100,
+		opts = {},
+		config = function()
+			require('plugins.tokyonight')
+			-- This must be after the setup to make it work
+			vim.cmd[[colorscheme tokyonight]]
+		end,
+	},
 	{ 'nvim-lua/plenary.nvim' },
 	{
 		'nvim-treesitter/nvim-treesitter',
