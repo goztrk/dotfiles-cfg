bf0312b64230b394c0f481da9bc121c6920cab77
goztrk <me@gokhan.org.tr>
2023-11-29 16:09:16 +0300

    Add ef alias to search with fzf and open with editor

--- .dotfiles/zsh/aliases.zsh
+++ .dotfiles/zsh/aliases.zsh
@@ -3,6 +3,8 @@ alias reload!='. ~/.zshrc'
 # Good 'ol Clear Screen command
 alias cls='clear'
 
+alias \?='alias | fzf'
+
 # Moving up from directory
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -10,6 +12,7 @@ alias ....='cd ../../..'
 alias cd..='cd ..'
 
 alias ee='emacsclient -nw -c'
+alias ef='e $(fzf)'
 
 alias cfg='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias lc='lazygit --git-dir=$HOME/.cfg --work-tree=$HOME'
