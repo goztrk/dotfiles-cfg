cc7a84b5a95fd231ed93e80d08a909f11d1179cb
goztrk <me@gokhan.org.tr>
2023-12-17 13:44:01 +0300

    nvim: add descriptions to keymappings

--- .config/nvim/lua/config/keymappings.lua
+++ .config/nvim/lua/config/keymappings.lua
@@ -33,40 +33,46 @@ keymap('v', 'x', '"_x', silent)
 keymap('v', 'X', '"_X', silent)
 
 -- Clear highlights
-keymap('n', '<leader>cl', '<cmd>nohl<cr>', silent)
+keymap('n', '<leader>cl', vim.cmd.nohl, { desc = 'Clear highlights', silent = true })
 
 -- Close buffer without exiting nvim
-keymap('n', '<leader>x', '<cmd>bd<CR>')
+keymap('n', '<leader>x', vim.cmd.bd, { desc = 'Close buffer' })
 
 -- Don't yank when pasting
-keymap('x', '<leader>p', '"_dP', silent)
+keymap('x', '<leader>p', '"_dP', { desc = 'Paste without yanking the old text', silent = true })
 
 -- Easy buffer switch
-keymap('n', '<Tab>', '<cmd>bnext<CR>')
-keymap('n', '<S-Tab>', '<cmd>bprev<CR>')
+keymap('n', '<Tab>', vim.cmd.bnext, { desc = 'Go to next buffer' })
+keymap('n', '<S-Tab>', vim.cmd.bprev, { desc = 'Go to previous buffer' })
 
-keymap('n', '<leader>cd', '<cmd>cd %:p:h<CR>')
+keymap('n', '<leader>cd', '<cmd>cd %:p:h<CR>', { desc = 'Change PWD to current file directory' })
 
 -- Telescope
-keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
-keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
-keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
-keymap('n', '<leader>fj', '<cmd>lua require("telescope").extensions.gitmoji.gitmoji()<cr>')
+keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = '[Telescope] Find files' })
+keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { desc = '[Telescope] Live grep' })
+keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = '[Telescope] Find in buffers' })
+keymap('n', '<leader>fk', '<cmd>Telescope keymaps<cr>', { desc = '[Telescope] Find keymaps' })
+keymap('n', '<leader>fj', '<cmd>lua require("telescope").extensions.gitmoji.gitmoji()<cr>',
+	{ desc = '[Telescope] Find gitmoji' })
 
 -- CTRL-w is annoying
 keymap('n', '<leader>w', '<c-w>', { noremap = true })
 
 -- Open nvim config in a new tab
-keymap('n', '<leader>/c', ':tabnew $MYVIMRC<cr> <bar> :tcd %:p:h<cr>', silent)
+keymap('n', '<leader>/c', ':tabnew $MYVIMRC<cr> <bar> :tcd %:p:h<cr>',
+	{ desc = 'Open Neovim config in a new tab', silent = true })
 
 -- LSP related
-keymap({'n', 'v'}, '<leader>lf', '<cmd>Format<cr>')
+keymap({ 'n', 'v' }, '<leader>lf', vim.cmd.Format, { desc = 'Format code' })
 
-keymap('n', '<leader>pv', vim.cmd.Ex)
+keymap('n', '<leader>pv', vim.cmd.Ex, { desc = 'Open Netrw' })
 
 -- Trouble
 keymap('n', '<leader>lx', '<cmd>TroubleToggle quickfix<cr>')
 
 -- Undotree
-keymap('n', '<leader>u', '<cmd>UndotreeToggle<cr>')
+keymap('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Open Undotree panel' })
 
+-- Ufo folding
+keymap('n', 'zC', require('ufo').closeAllFolds)
+keymap('n', 'zA', require('ufo').openAllFolds)
